95cf054446c0d402414c9d5fcb899a786bec0e33
mbugua <mbuguafaimba@gmail.com>
2023-03-20 23:11:16 +0300

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /birds
   def index
@@ -55,10 +56,17 @@ class BirdsController < ApplicationController
     end
   end
 
-  private
+private
+ def find_bird
+    Bird.find(params[:id])
+  end
 
   def bird_params
     params.permit(:name, :species, :likes)
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
   end
 
 end
